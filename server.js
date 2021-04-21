f0e3f2447a358397c94904e2aeebbccde359e1a3
nicholasstine <nickstine1450@gmail.com>
2021-04-20 21:27:47 -0700

    Cors Reconfig

--- server.js
+++ server.js
@@ -8,17 +8,22 @@ const fileRoutes = require('./routes/fileRoutes');
 require('dotenv').config()
 require('./db/psqlSetup')();
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+//     next();
+// });
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/user', userRoutes);
+app.use('/user', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    next();
+}, userRoutes);
 app.use('/file', fileRoutes);
 
 app.listen(process.env.PORT, () => {
