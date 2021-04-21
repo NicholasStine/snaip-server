47fc1264041c5a2cec65593bc94ea03c26043204
nicholasstine <nickstine1450@gmail.com>
2021-04-20 21:25:08 -0700

    Cors Reconfig

--- server.js
+++ server.js
@@ -5,16 +5,17 @@ const app = express();
 const userRoutes = require('./routes/userRoutes');
 const fileRoutes = require('./routes/fileRoutes');
 
-app.use(cors());
 require('dotenv').config()
 require('./db/psqlSetup')();
 
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-//     next();
-// });
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    next();
+});
+
+app.use(cors());
 app.use(express.json());
 
 app.use('/user', userRoutes);
