d65b398d6dcf5faad69ee3edba9e4a88117d965b
nicholasstine <nickstine1450@gmail.com>
2021-04-30 22:00:13 -0700

    yet another attempt at heroku cors

--- server.js
+++ server.js
@@ -21,9 +21,10 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/user', (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
 }, userRoutes);
 app.use('/file', fileRoutes);
